3e9265173c60b2ee169accea945995ed6f898637
draculajun <34988153@qq.com>
2019-11-10 11:37:53 +0800

    springcloudconfig; eureka;

--- src/main/java/activiti/ActivitiDemoApplication.java
+++ src/main/java/activiti/ActivitiDemoApplication.java
@@ -6,12 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableTransactionManagement
 @MapperScan(basePackages = "activiti/mapperdao")
-@RestController
 @EnableEurekaClient
 public class ActivitiDemoApplication {
 
